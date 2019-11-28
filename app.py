45946cbd8cd88fe50448af5c5284f167e4fe3641
sonukiran <sonukiran44@gmail.com>
2019-11-28 13:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
+    render_template('index.html', time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
