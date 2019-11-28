1844c49de1be03455a7014ba67eabdea0aabcfcf
sonukiran <sonukiran44@gmail.com>
2019-11-28 14:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-    render_template('index.html', time=the_time)
+    return render_template('index.html', time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
