253c748162da444cae8cfdfad9cf5bb7ad11520f
sonukiran <sonukiran44@gmail.com>
2019-11-28 13:42:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def homepage():
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
 if __name__ == '__main__':
